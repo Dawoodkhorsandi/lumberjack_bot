e6c6e780ea03a4d7659e183a5a24cabdf2624711
Your Name <dk.marivani@gmail.com>
2018-07-27 02:46:21 +0430

    adding pictures for image recognitions

--- lumberjack_bot.py
+++ lumberjack_bot.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import os
 # 2 seconds for switching to LumberJack Tab
 time.sleep(2)
 
@@ -13,18 +14,18 @@ pyautogui.click()
 
 
 while True:
-    if pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\left.png",region=(597,326,25,27)) != None:
+    if pyautogui.locateOnScreen(r"images\left.png",region=(597,326,25,27)) != None:
         # pyautogui.moveTo(766,603)
         # pyautogui.click()
         pyautogui.press("right")
 
-    elif pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\right.png",region =(743,308,29,36)) != None:
+    elif pyautogui.locateOnScreen(r"images\right.png",region =(743,308,29,36)) != None:
         # pyautogui.moveTo(600,606)
         # pyautogui.click()
         # pyautogui.click()
         pyautogui.press("left")
 
-    elif pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\tworights.png",region =(725,247,34,130)) != None:
+    elif pyautogui.locateOnScreen(r"images\tworights.png",region =(725,247,34,130)) != None:
         # pyautogui.moveTo(766,603)
         # pyautogui.click()
         # pyautogui.click()
@@ -32,7 +33,7 @@ while True:
         pyautogui.press("right")
 
 
-    elif pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\twolefts.png",region =(605,276,38,93)) != None:
+    elif pyautogui.locateOnScreen(r"images\twolefts.png",region =(605,276,38,93)) != None:
         # pyautogui.moveTo(600,606)
         # pyautogui.click()
         # pyautogui.click()
@@ -41,7 +42,7 @@ while True:
 
 
 
-    elif pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\oneleftthenright.png",region =(598,277,48,67)) != None:
+    elif pyautogui.locateOnScreen(r"images\oneleftthenright.png",region =(598,277,48,67)) != None:
         # pyautogui.moveTo(600,606)
         # pyautogui.click()
         pyautogui.press("left")
@@ -50,7 +51,7 @@ while True:
         pyautogui.press("right")
 
 
-    elif pyautogui.locateOnScreen(r"c:\users\kian\desktop\lumberjack\leftorright\onerightthenleft.png",region =(714,275,41,72)) != None:
+    elif pyautogui.locateOnScreen(r"images\onerightthenleft.png",region =(714,275,41,72)) != None:
         # pyautogui.moveTo(766,603)
         # pyautogui.click()
         pyautogui.press("right")
